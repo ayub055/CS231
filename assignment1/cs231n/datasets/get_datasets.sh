9d2b064e594ae5d9b916c3feb82d1a73e3e6f777
Mohd Ayyoob <ayyoobmohd@iisc.ac.in>
2025-08-10 17:23:56 +0530

    checking

--- assignment1/cs231n/datasets/get_datasets.sh
+++ assignment1/cs231n/datasets/get_datasets.sh
@@ -1,8 +1,27 @@
-# cd cs231n/datasets
-if [ ! -d "cifar-10-batches-py" ]; then
-  wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -O cifar-10-python.tar.gz
-  tar -xzvf cifar-10-python.tar.gz
-  rm cifar-10-python.tar.gz
-  wget http://cs231n.stanford.edu/imagenet_val_25.npz
+# # cd cs231n/datasets
+# if [ ! -d "cifar-10-batches-py" ]; then
+#   wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -O cifar-10-python.tar.gz
+#   tar -xzvf cifar-10-python.tar.gz
+#   rm cifar-10-python.tar.gz
+#   wget http://cs231n.stanford.edu/imagenet_val_25.npz
+# fi
+# # cd ../..
+
+NEW_EMAIL="ayyoobmohd@iisc.ac.in"
+OLD_EMAIL="ayyoob@Mohds-MacBook-Air.local"
+NEW_NAME="Mohd Ayyoob"   # keep your name same or change if needed
+
+git filter-branch --env-filter '
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-# cd ../..
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
